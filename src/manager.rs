a8f8e1b7e3f42a6422028c086d95efeed30b25b7
PaoloRollo <paolo.rollo@laserromae.it>
2020-05-20 10:08:43 +0200

    added missing manager file.

---
+++ src/manager.rs
@@ -0,0 +1,44 @@
+use chashmap::CHashMap;
+
+use crate::queue::TinQueue;
+
+pub struct TinQueueManager {
+    map: CHashMap<String, TinQueue<String>>
+}
+
+impl TinQueueManager {
+    // Create new Tin Queue Manager
+    pub fn new() -> Self {
+        Self {
+            map: CHashMap::new(),
+        }
+    }
+
+    // Add new Tin Queue to the map
+    pub fn add_queue(&self, queue_name: String, size: usize) -> Option<TinQueue<String>> {
+        let queue = TinQueue::new(size);
+        self.map.insert(queue_name, queue)
+    }
+
+    // Get Tin Queue from the manager
+    pub fn get_queue(&self, queue_name: String) -> Option<TinQueue<String>> {
+        match &mut self.map.get_mut(&queue_name) {
+            Some(queue) => {
+                return Some(queue.to_owned());
+            },
+            None => {
+                None
+            }
+        }
+    }
+
+    // Update Tin Queue
+    pub fn update_queue(&self, queue_name: String, queue: TinQueue<String>) {
+        self.map.insert(queue_name, queue);
+    }
+
+    // Delete a Tin Queue from the manager
+    pub fn delete_queue(&self, queue_name: String) -> Option<TinQueue<String>> {
+        self.map.remove(&queue_name)
+    }
+}
